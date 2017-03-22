e11aefe44d5390163fd271460e1984eb9b4ea9b3
mangotonk <scott.maclaren@junction6travel.com>
2017-03-22 17:24:56 +1300

    adding folder

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/code/GatewayInfo.php';
+//require_once __DIR__ . '/code/GatewayInfo.php';
 //function __autoload($class_name) {
 //    require_once('code/'.$class_name.'.php');
 //}
