0e86206c346d88bff3f82e26932b3b75ac29939c
mangotonk <scott.maclaren@junction6travel.com>
2017-03-27 10:31:05 +1300

    Pin Field Extension

--- _config.php
+++ _config.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ .'/code/include_omnipay.php';
 
+Object::add_extension('FormField', 'PinFormExtension');
+
 //require_once __DIR__ . '/code/GatewayInfo.php';
 //function __autoload($class_name) {
 //    require_once('code/'.$class_name.'.php');
